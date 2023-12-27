997d036b9d4fc8742f32c4cc6e1e22a899c2a083
Jay Ramoliya <ramoliyajay9@gmail.com>
2023-12-27 11:19:57 +0530

    GSAP

--- PORT/script.js
+++ PORT/script.js
@@ -57,3 +57,70 @@ tl.to('#loader',{
     delay:0.5,
     duration:0.8
 })
+
+
+gsap.from("#main .user_info .user_name", {
+    x: -700,
+    duration: 2,
+    delay: 1,
+    stagger: 0.4,
+    scrollTrigger:{
+        trigger:"#main",
+        scroller:'body',
+        markers:true,
+        start:'top 50%',
+        end:'top 30%',
+        // scrub:true // two value aapi saki 
+        scrub:2 // 1 ane 5 ni vache
+    }
+})
+
+gsap.from(".about_heading,.about p,.about img", {
+    scale: 0.2,
+    opacity: 0,
+    duration: 0.5,
+    stagger: 0.4,
+    scrollTrigger:{
+        trigger:".about",
+        scroller:'body',
+        markers:true,
+        start:'top 80%',
+        end:'top 30%',
+        // scrub:true // two value aapi saki 
+        scrub:2 // 1 ane 5 ni vache
+    }
+})
+
+gsap.from("#main .user_img img", {
+    scale: 0.2,
+    opacity: 0,
+    duration: 0.5,
+    delay: 1,
+    stagger: 0.4,
+    scrollTrigger:{
+        trigger:"#main",
+        scroller:'body',
+        markers:true,
+        start:'top 50%',
+        end:'top 30%',
+        // scrub:true // two value aapi saki 
+        scrub:2 // 1 ane 5 ni vache
+    }
+})
+
+gsap.from(".skill_box .box", {
+    scale: 0.2,
+    opacity: 0,
+    duration: 0.5,
+    delay: 1,
+    stagger: 0.1,
+    scrollTrigger:{
+        trigger:".skills",
+        scroller:'body',
+        markers:true,
+        start:'top 60%',
+        end:'top 30%',
+        // scrub:true // two value aapi saki 
+        scrub:2 // 1 ane 5 ni vache
+    }
+})
