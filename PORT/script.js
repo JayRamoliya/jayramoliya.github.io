936c9d7f1d58d7ae9795a76a3ecc1ef945768164
Jay Ramoliya <ramoliyajay9@gmail.com>
2023-12-29 10:24:48 +0530

    Cursor

--- PORT/script.js
+++ PORT/script.js
@@ -1,61 +1,63 @@
+
+
 var tl = gsap.timeline()
 
-tl.from('nav',{
-    opacity:0,
-    delay:0.3
+tl.from('nav', {
+    opacity: 0,
+    delay: 0.3
 })
 
 
-tl.from('nav h1,nav h4,nav h3',{
-    y:-80,
-    opacity:0,
-    duration:0.8,
-    delay:0.5,
-    stagger:0.3
+tl.from('nav h1,nav h4,nav h3', {
+    y: -80,
+    opacity: 0,
+    duration: 0.8,
+    delay: 0.5,
+    stagger: 0.3
 })
 
-tl.from('#left h1',{
-    x:-500,
-    opacity:0,
-    duration:0.5,
-    stagger:0.4 // use to element aek pachi aek ave
+tl.from('#left h1', {
+    x: -500,
+    opacity: 0,
+    duration: 0.5,
+    stagger: 0.4 // use to element aek pachi aek ave
 })
 
-tl.from('#right img',{
-    scale:0.2,
-    opacity:0,
-    duration:0.5
+tl.from('#right img', {
+    scale: 0.2,
+    opacity: 0,
+    duration: 0.5
 })
 
-var tl=gsap.timeline()
+var tl = gsap.timeline()
 // main note agr niche no code tame pachi lagvso to nai chale
 
-function time(){
+function time() {
     var a = 0
-    setInterval(function(){
-        a = a + Math.floor(Math.random()*15)
-        if(a<100){
-            document.querySelector('#loader h1').innerHTML=a+"%"
+    setInterval(function () {
+        a = a + Math.floor(Math.random() * 15)
+        if (a < 100) {
+            document.querySelector('#loader h1').innerHTML = a + "%"
         }
-        else{
-            a=100
-            document.querySelector('#loader h1').innerHTML=a+"%"
+        else {
+            a = 100
+            document.querySelector('#loader h1').innerHTML = a + "%"
         }
-    },100)
+    }, 100)
 }
 // time()
 
-tl.to('#loader h1',{
-    scale:0.8,
-    delay:0.3,
-    duration:0.9,
-    onStart:time()
+tl.to('#loader h1', {
+    scale: 0.8,
+    delay: 0.3,
+    duration: 0.9,
+    onStart: time()
 })
 
-tl.to('#loader',{
-    top:'-100vh',
-    delay:0.5,
-    duration:0.8
+tl.to('#loader', {
+    top: '-100vh',
+    delay: 0.5,
+    duration: 0.8
 })
 
 
@@ -64,14 +66,14 @@ gsap.from("#main .user_info .user_name", {
     duration: 2,
     delay: 1,
     stagger: 0.4,
-    scrollTrigger:{
-        trigger:"#main",
-        scroller:'body',
-        markers:true,
-        start:'top 50%',
-        end:'top 30%',
+    scrollTrigger: {
+        trigger: "#main",
+        scroller: 'body',
+        markers: true,
+        start: 'top 50%',
+        end: 'top 30%',
         // scrub:true // two value aapi saki 
-        scrub:2 // 1 ane 5 ni vache
+        scrub: 2 // 1 ane 5 ni vache
     }
 })
 
@@ -80,14 +82,14 @@ gsap.from(".about_heading,.about p,.about img", {
     opacity: 0,
     duration: 0.5,
     stagger: 0.4,
-    scrollTrigger:{
-        trigger:".about",
-        scroller:'body',
-        markers:true,
-        start:'top 80%',
-        end:'top 30%',
+    scrollTrigger: {
+        trigger: ".about",
+        scroller: 'body',
+        markers: true,
+        start: 'top 80%',
+        end: 'top 30%',
         // scrub:true // two value aapi saki 
-        scrub:2 // 1 ane 5 ni vache
+        scrub: 2 // 1 ane 5 ni vache
     }
 })
 
@@ -97,14 +99,14 @@ gsap.from("#main .user_img img", {
     duration: 0.5,
     delay: 1,
     stagger: 0.4,
-    scrollTrigger:{
-        trigger:"#main",
-        scroller:'body',
-        markers:true,
-        start:'top 50%',
-        end:'top 30%',
+    scrollTrigger: {
+        trigger: "#main",
+        scroller: 'body',
+        markers: true,
+        start: 'top 50%',
+        end: 'top 30%',
         // scrub:true // two value aapi saki 
-        scrub:2 // 1 ane 5 ni vache
+        scrub: 2 // 1 ane 5 ni vache
     }
 })
 
@@ -114,13 +116,15 @@ gsap.from(".skill_box .box", {
     duration: 0.5,
     delay: 1,
     stagger: 0.1,
-    scrollTrigger:{
-        trigger:".skills",
-        scroller:'body',
-        markers:true,
-        start:'top 60%',
-        end:'top 30%',
+    scrollTrigger: {
+        trigger: ".skills",
+        scroller: 'body',
+        markers: true,
+        start: 'top 60%',
+        end: 'top 30%',
         // scrub:true // two value aapi saki 
-        scrub:2 // 1 ane 5 ni vache
+        scrub: 2 // 1 ane 5 ni vache
     }
 })
+
+
